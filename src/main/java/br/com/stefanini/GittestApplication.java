0decff3f2c5c7d59ee8637b7839cfef2b262ca2f
Jose Carlos M Oliveira Junior <josecmoj@gmail.com>
2017-03-22 15:04:11 -0300

    Adicionados os arquivos do projeto

---
+++ src/main/java/br/com/stefanini/GittestApplication.java
@@ -0,0 +1,12 @@
+package br.com.stefanini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittestApplication.class, args);
+	}
+}
